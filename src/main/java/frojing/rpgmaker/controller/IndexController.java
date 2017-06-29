77d9d68b221ebbe77d6655402a8f4a4db9db8b00
igorfachini <igor.fachini@catolicasc.org.br>
2017-06-29 02:24:10 -0300

    Fonts fixed and started index layout

--- src/main/java/frojing/rpgmaker/controller/IndexController.java
+++ src/main/java/frojing/rpgmaker/controller/IndexController.java
@@ -1,13 +1,26 @@
 package frojing.rpgmaker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import frojing.rpgmaker.data.PlayerDao;
+import frojing.rpgmaker.data.ChronicleDao;
+import org.springframework.ui.Model;
 
 @Controller
 public class IndexController {
 
+	@Autowired
+	private ChronicleDao dao;
+	@Autowired
+	private PlayerDao playerDao;
+
 	@RequestMapping("/")
-	String index() {
+	public String index(Model model) {
+		model.addAttribute("chronicles", dao.findAll());
+		model.addAttribute("chroniclesJoin", dao.findAll());
+		model.addAttribute("chroniclesMaster", dao.findAll());
+		System.out.println("Returning chronicles:");
 		return "index";
 	}
 
